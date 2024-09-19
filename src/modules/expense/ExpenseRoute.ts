d8c0de140dda833f77271f2aef07c24b035a8279
rafmasloman <raflymasloman20@gmail.com>
2024-09-19 18:41:45 +0800

    feat : create expense modules mvc

---
+++ src/modules/expense/ExpenseRoute.ts
@@ -0,0 +1,69 @@
+import { Router } from 'express';
+import ExpenseController from './ExpenseController';
+import { authMiddleware } from '../../middleware/auth.middleware';
+
+class ExpenseRoute {
+  private expenseController: ExpenseController;
+  private route: Router;
+
+  constructor(expenseController: ExpenseController, route: Router) {
+    this.expenseController = expenseController;
+    this.route = route;
+    this.registerRoute();
+  }
+
+  private createExpenseRouter() {
+    return this.route.post(
+      '/',
+      authMiddleware,
+      this.expenseController.createExpense,
+    );
+  }
+
+  private getAllExpensesRouter() {
+    return this.route.get(
+      '/',
+      authMiddleware,
+      this.expenseController.getAllExpenses,
+    );
+  }
+
+  private getExpenseDetail() {
+    return this.route.get(
+      '/:id',
+      authMiddleware,
+      this.expenseController.getDetailExpense,
+    );
+  }
+
+  private updateExpenseDetail() {
+    return this.route.put(
+      '/:id',
+      authMiddleware,
+      this.expenseController.updateExpense,
+    );
+  }
+
+  private deleteExpense() {
+    return this.route.delete(
+      '/:id',
+      authMiddleware,
+      this.expenseController.deleteExpense,
+    );
+  }
+
+  registerRoute() {
+    this.createExpenseRouter();
+    this.getAllExpensesRouter();
+    this.getExpenseDetail();
+    this.deleteExpense();
+
+    return this.route;
+  }
+
+  getRouter() {
+    return this.route;
+  }
+}
+
+export default ExpenseRoute;
