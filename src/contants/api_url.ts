84d90d9b56f9073ffc6e04a6cd4da6e73aa48045
rafmasloman <raflymasloman20@gmail.com>
2024-09-19 18:36:20 +0800

    feat : create url constant value

---
+++ src/contants/api_url.ts
@@ -0,0 +1,7 @@
+export const BASE_API_URL = '/api/v1';
+export const EXPENSE = '/expense';
+export const EXPENSE_CATEGORY = '/expense-category';
+export const INCOME = '/income';
+export const SUPPLIER = '/supplier';
+export const SUPPLIER_COMPANY = '/supplier-company';
+export const USER = '/user';
