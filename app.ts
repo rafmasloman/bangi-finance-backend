eff7d235e724591709015dcca088ec5006f85b5d
rafmasloman <raflymasloman20@gmail.com>
2024-09-19 18:35:56 +0800

    feat : create router config

---
+++ app.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import express, { Express } from 'express';
+
+import cors from 'cors';
+import http from 'http';
+import cookieParser from 'cookie-parser';
+import routes from './src/config/routes.config';
+
+dotenv.config();
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.use('/api/v1', routes);
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, () => {
+  console.log('server is running on port : ', process.env.PORT);
+});
---
+++ src/config/routes.config.ts
@@ -0,0 +1,45 @@
+import { Router } from 'express';
+import AuthService from '../modules/auth/AuthService';
+import AuthController from '../modules/auth/AuthController';
+import AuthRoute from '../modules/auth/AuthRoute';
+import UserService from '../modules/user/UserService';
+import UserController from '../modules/user/UserController';
+import UserRoute from '../modules/user/UserRoute';
+import ExpenseCategoryService from '../modules/expense-category/ExpenseCategoryService';
+import ExpenseCategoryController from '../modules/expense-category/ExpenseCategoryController';
+import ExpenseCategoryRoute from '../modules/expense-category/ExpenseCategoryRoute';
+import SupplierService from '../modules/supplier/SupplierService';
+import SupplierController from '../modules/supplier/SupplierController';
+import SupplierRoute from '../modules/supplier/SupplierRoute';
+
+const routes = Router();
+
+// Inisialisasi services dan controllers
+const AuthServices = new AuthService();
+const UserServices = new UserService();
+const SupplierServices = new SupplierService();
+const ExpenseCategoryServices = new ExpenseCategoryService();
+
+const AuthControllers = new AuthController(AuthServices);
+const UserControllers = new UserController(UserServices);
+const SupplierControllers = new SupplierController(SupplierServices);
+const ExpenseCategoryControllers = new ExpenseCategoryController(
+  ExpenseCategoryServices,
+);
+
+// Route Definitions
+const AuthRoutes = new AuthRoute(AuthControllers, routes);
+const UserRoutes = new UserRoute(UserControllers, routes);
+const ExpenseCategoryRoutes = new ExpenseCategoryRoute(
+  ExpenseCategoryControllers,
+  routes,
+);
+const SupplierRoutes = new SupplierRoute(SupplierControllers, routes);
+
+// Daftarkan route di sini tanpa prefix
+routes.use('/auth', AuthRoutes.getRoute());
+routes.use('/user', UserRoutes.getRoute());
+routes.use('/expense-category', ExpenseCategoryRoutes.getRoutes());
+routes.use('/supplier', SupplierRoutes.registerRoute());
+
+export default routes;
