8173d8ae4ccf9106a352c9a8232e5d811c408b8c
rafmasloman <raflymasloman20@gmail.com>
2024-09-20 19:11:06 +0800

    feat : create constant message response

---
+++ src/contants/message_response.ts
@@ -0,0 +1,15 @@
+export const READ_INCOMES_MESSAGE = 'Incomes fetched succesfully';
+export const READ_INCOME_DETAIL_MESSAGE = 'Income detail fetched succesfully';
+export const READ_INCOME_TOTAL_EACH_DATA_MESSAGE =
+  'Incomes total each fetched succesfully';
+export const CREATE_INCOMES_MESSAGE = 'Incomes created succesfully';
+export const UPDATE_INCOMES_MESSAGE = 'Incomes updated succesfully';
+export const DELETE_INCOMES_MESSAGE = 'Incomes deleted succesfully';
+
+export const READ_EXPENSES_MESSAGE = 'expenses fetched succesfully';
+export const READ_EXPENSE_DETAIL_MESSAGE = 'expense detail fetched succesfully';
+export const READ_EXPENSE_TOTAL_EACH_DATA_MESSAGE =
+  'expenses total each fetched succesfully';
+export const CREATE_EXPENSES_MESSAGE = 'expenses created succesfully';
+export const UPDATE_EXPENSES_MESSAGE = 'expenses updated succesfully';
+export const DELETE_EXPENSES_MESSAGE = 'expenses deleted succesfully';
