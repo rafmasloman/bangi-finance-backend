03fbbcc0182de68798cd2a4b7f2b140d61a16b6a
rafmasloman <raflymasloman20@gmail.com>
2024-09-20 19:11:23 +0800

    feat : create response helper

---
+++ src/helpers/response.helper.ts
@@ -0,0 +1,16 @@
+import { Response } from 'express';
+
+export const sendSuccessResponse = (
+  res: Response,
+  data: any,
+  message: string = 'Request Succesfull',
+  statusCode: number = 200,
+) => {
+  return res.json({
+    status: 'success',
+    message,
+    data,
+    statusCode,
+    timeStamp: new Date().toISOString(),
+  });
+};
